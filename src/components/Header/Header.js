229ee048b3dd7e7615f168de213580f82f2b96e6
Erica Kresch <ekresch15@gmail.com>
2021-01-13 15:48:30 -0600

    adding header and getting rid of bootstrap.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,90 +1,20 @@
-// export default Header;
-
-// import React from "react";
-// import Navbar from "react-materialize";
-// import NavItem from "react-materialize";
-
-// function Header() {
-//   return (
-//     <Navbar brand="logo" left>
-//       <NavItem><Link to="/Home" activeClassName="active">Home</Link></NavItem>
-//         <NavItem><Link to="/Sign-In" activeClassName="active">Sign In</Link></NavItem>
-//         <NavItem><Link to="/Register" activeClassName="active">Register</Link></NavItem>
-//     </Navbar>
-//   );
-// }
-// export default Header;
-
-// function Header() {
-//   return(
-//     <nav>
-//     <div class="nav-wrapper">
-//       <p>Logo</p>
-//       <p>Section 1</p>
-//       {/* <ul id="nav-mobile" class="right hide-on-med-and-down">
-//         <li>Item 1</li>
-//         <li>Item 2</li>
-//         <li>Item 3</li>
-//       </ul> */}
-//     </div>
-//   </nav>
-//   );
-// }
-
-// export default Header;
-
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
 import {Link} from 'react-router-dom';
+import './Header.css';
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
 
 function Header() {
-  const classes = useStyles();
-
   return (
-
-    <header>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/restaurant">Restaurant</Link></li>
-          {/* <li><Link to={{
-            pathname: '/restaurant',
-            hash: '#submit',
-            search: '?quick-submit=true'
-          }}>Restaurant</Link></li> */}
-        </ul>
-      </nav>
-    </header>
-
-    // <AppBar position="static">
-    // <Toolbar>
-    //   <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-    //     <MenuIcon />
-    //   </IconButton>
-    //   <Typography variant="h6" className={classes.title}>
-    //     Enca Eats
-    //   </Typography>
-    //   <Button color="inherit">Login</Button>
-    // </Toolbar>
-    // </AppBar>
+      <div>
+        <header className="NavHeader">
+          <nav>
+            <ul>
+              <li><Link className="header" to="/">Home</Link></li>
+              <li><Link className="header" to="/restaurant">Restaurant</Link></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
   );
 }
 
--- src/components/RestaurantCard/RestaurantCard.js
+++ src/components/RestaurantCard/RestaurantCard.js
@@ -1,8 +1,5 @@
 import "./RestaurantCard.css";
 import GenericButton from '../GenericButton/GenericButton'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
 import React from "react";
 
 
@@ -16,18 +13,16 @@ function RestaurantCard(props) {
     });
 
     return (
-        <Container className="restaurantCard" fluid>
-            <Row className="restaurantRow">
-                <Col><img alt="yelpPic" className="thumbnail" src={image_url} /></Col>
-                <Col>{name}</Col>
-                <Col>{neighborhood}</Col>
-                <Col>{rating}</Col>
-                <Col>{cat_titles.join(", ")}</Col>
-                <Col><a target='_blank' href={url}>Yelp Page</a></Col>
-                <Col><GenericButton index={props.index} type="wishList" bool={wishList.bool} toggleHandler={props.toggleHandler} /></Col>
-                <Col><GenericButton index={props.index} type="visited" bool={visited.bool} toggleHandler={props.toggleHandler} /></Col>
-            </Row>
-        </Container>
+        <div className="restaurantCard">
+            <div><img alt="yelpPic" className="thumbnail" src={image_url} /></div>
+            <div>{name}</div>
+            <div>{neighborhood}</div>
+            <div>{rating}</div>
+            <div>{cat_titles.join(", ")}</div>
+            <div><a target='_blank' href={url}>Yelp Page</a></div>
+            <div><GenericButton index={props.index} type="wishList" bool={wishList.bool} toggleHandler={props.toggleHandler} /></div>
+            <div><GenericButton index={props.index} type="visited" bool={visited.bool} toggleHandler={props.toggleHandler} /></div>
+        </div>
     );
 
 }
