9857c561a2c056e3b2f8165013ed3be62e1d3b75
Michael Ferguson <mikeferg2011@gmail.com>
2021-01-22 17:34:34 -0500

    get dropdowns from backend

--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from "react";
 import SearchResults from "../../components/SearchResults/SearchResults";
-import SearchDropdowns from '../../components/SearchDropdowns/SearchDropdowns'
+import SearchDropdowns from "../../components/SearchDropdowns/SearchDropdowns";
+import API from "../../utils/Api";
 
 // function App() {
 class SearchPage extends Component {
-  constructor() {
-    super()
-    this.state = {
-      restaurants: [{
+  state = {
+    restaurants: [
+      {
         id: "TAueIovlJFslHSVEfnAaaA",
         alias: "dryhop-brewers-chicago",
         name: "DryHop Brewers",
-        image_url: "https://s3-media4.fl.yelpcdn.com/bphoto/Eb0McDi48qWYhPnIaK8i-Q/o.jpg",
+        image_url:
+          "https://s3-media4.fl.yelpcdn.com/bphoto/Eb0McDi48qWYhPnIaK8i-Q/o.jpg",
         is_closed: false,
-        url: "https://www.yelp.com/biz/dryhop-brewers-chicago?adjust_creative=ZzZ3ffcKnwQmN827qh1vKw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=ZzZ3ffcKnwQmN827qh1vKw",
+        url:
+          "https://www.yelp.com/biz/dryhop-brewers-chicago?adjust_creative=ZzZ3ffcKnwQmN827qh1vKw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=ZzZ3ffcKnwQmN827qh1vKw",
         review_count: 771,
         categories: [
           {
             alias: "breweries",
-            title: "Breweries"
+            title: "Breweries",
           },
           {
             alias: "newamerican",
-            title: "American (New)"
-          }
+            title: "American (New)",
+          },
         ],
         rating: 4,
         coordinates: {
           latitude: 41.9392443174878,
-          longitude: -87.6442063962951
+          longitude: -87.6442063962951,
         },
-        transactions: [
-          "delivery",
-          "pickup"
-        ],
+        transactions: ["delivery", "pickup"],
         price: "$$",
         location: {
           address1: "3155 N Broadway",
@@ -43,10 +42,7 @@ class SearchPage extends Component {
           zip_code: "60657",
           country: "US",
           state: "IL",
-          display_address: [
-            "3155 N Broadway",
-            "Chicago, IL 60657"
-          ]
+          display_address: ["3155 N Broadway", "Chicago, IL 60657"],
         },
         phone: "+17738573155",
         display_phone: "(773) 857-3155",
@@ -54,42 +50,41 @@ class SearchPage extends Component {
         neighborhood: "Lake View",
         _rid: "tJleAOrMycgxCAAAAAAAAA==",
         _self: "dbs/tJleAA==/colls/tJleAOrMycg=/docs/tJleAOrMycgxCAAAAAAAAA==/",
-        _etag: "\"bd01f2de-0000-0300-0000-5ff228f20000\"",
+        _etag: '"bd01f2de-0000-0300-0000-5ff228f20000"',
         _attachments: "attachments/",
         _ts: 1609705714,
         userInteractions: {
-          wish_list: { date: "2021-01-01 10:18:44", bool: true },
+          wish_list: {date: "2021-01-01 10:18:44", bool: true},
           visited: {},
-          notes: []
-        }
+          notes: [],
+        },
       },
       {
         id: "TAueIovlJFslHSVEfnAaaA",
         alias: "dryhop-brewers-chicago",
         name: "DryHop Brewers",
-        image_url: "https://s3-media4.fl.yelpcdn.com/bphoto/Eb0McDi48qWYhPnIaK8i-Q/o.jpg",
+        image_url:
+          "https://s3-media4.fl.yelpcdn.com/bphoto/Eb0McDi48qWYhPnIaK8i-Q/o.jpg",
         is_closed: false,
-        url: "https://www.yelp.com/biz/dryhop-brewers-chicago?adjust_creative=ZzZ3ffcKnwQmN827qh1vKw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=ZzZ3ffcKnwQmN827qh1vKw",
+        url:
+          "https://www.yelp.com/biz/dryhop-brewers-chicago?adjust_creative=ZzZ3ffcKnwQmN827qh1vKw&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=ZzZ3ffcKnwQmN827qh1vKw",
         review_count: 771,
         categories: [
           {
             alias: "breweries",
-            title: "Breweries"
+            title: "Breweries",
           },
           {
             alias: "newamerican",
-            title: "American (New)"
-          }
+            title: "American (New)",
+          },
         ],
         rating: 4,
         coordinates: {
           latitude: 41.9392443174878,
-          longitude: -87.6442063962951
+          longitude: -87.6442063962951,
         },
-        transactions: [
-          "delivery",
-          "pickup"
-        ],
+        transactions: ["delivery", "pickup"],
         price: "$$",
         location: {
           address1: "3155 N Broadway",
@@ -99,10 +94,7 @@ class SearchPage extends Component {
           zip_code: "60657",
           country: "US",
           state: "IL",
-          display_address: [
-            "3155 N Broadway",
-            "Chicago, IL 60657"
-          ]
+          display_address: ["3155 N Broadway", "Chicago, IL 60657"],
         },
         phone: "+17738573155",
         display_phone: "(773) 857-3155",
@@ -110,81 +102,120 @@ class SearchPage extends Component {
         neighborhood: "Lake View",
         _rid: "tJleAOrMycgxCAAAAAAAAA==",
         _self: "dbs/tJleAA==/colls/tJleAOrMycg=/docs/tJleAOrMycgxCAAAAAAAAA==/",
-        _etag: "\"bd01f2de-0000-0300-0000-5ff228f20000\"",
+        _etag: '"bd01f2de-0000-0300-0000-5ff228f20000"',
         _attachments: "attachments/",
         _ts: 1609705714,
         userInteractions: {
-          wish_list: { date: "2021-01-01 10:18:44", bool: false },
+          wish_list: {date: "2021-01-01 10:18:44", bool: false},
           visited: {},
-          notes: []
-        }
-      }],
-      dropdown: {
-        neighborhoods: {
-          all: [
-            { alias: 'Gold Coast', title: 'Gold Coast' },
-            { alias: "Lake View", title: "Lake View" },
-            { alias: "Lincoln Park", title: "Lincoln Park" },
-            { alias: "Wrigleyville", title: "Wrigleyville" },
-          ],
-          selected: []
+          notes: [],
         },
-        category: {
-          all: [
-            { alias: "burgers", title: "Burgers" },
-            { alias: "bbq", title: "Barbeque" }],
-          selected: []
-        },
-        rating: {
-          all: [
-            { alias: 0, title: 0 },
-            { alias: 0.5, title: 0.5 },
-            { alias: 1, title: 1 },
-            { alias: 1.5, title: 1.5 },
-            { alias: 2, title: 2 },
-            { alias: 2.5, title: 2.5 },
-            { alias: 3, title: 3 },
-            { alias: 3.5, title: 3.5 },
-            { alias: 4, title: 4 },
-            { alias: 4.5, title: 4.5 },
-            { alias: 5, title: 5 },
-          ],
-          selected: []
-        },
-        wish_list: {
-          all: [
-            { alias: 'No', title: 'No' },
-            { alias: 'Yes', title: 'Yes' }
-          ],
-          selected: []
-        },
-        visited: {
-          all: [
-            { alias: 'No', title: 'No' },
-            { alias: 'Yes', title: 'Yes' }
-          ],
-          selected: []
-        },
-        price: {
-          all: [
-            { alias: '$', title: '$' },
-            { alias: '$$', title: '$$' },
-            { alias: '$$$', title: '$$$' },
-            { alias: '$$$$', title: '$$$$' },
-          ],
-          selected: []
-        }
-      }
+      },
+    ],
+    dropdown: {
+      neighborhoods: {
+        all: [],
+        selected: [],
+      },
+      categories: {
+        all: [],
+        selected: [],
+      },
+      rating: {
+        all: [
+          {alias: 0, title: 0},
+          {alias: 0.5, title: 0.5},
+          {alias: 1, title: 1},
+          {alias: 1.5, title: 1.5},
+          {alias: 2, title: 2},
+          {alias: 2.5, title: 2.5},
+          {alias: 3, title: 3},
+          {alias: 3.5, title: 3.5},
+          {alias: 4, title: 4},
+          {alias: 4.5, title: 4.5},
+          {alias: 5, title: 5},
+        ],
+        selected: [],
+      },
+      wish_list: {
+        all: [
+          {alias: "No", title: "No"},
+          {alias: "Yes", title: "Yes"},
+        ],
+        selected: [],
+      },
+      visited: {
+        all: [
+          {alias: "No", title: "No"},
+          {alias: "Yes", title: "Yes"},
+        ],
+        selected: [],
+      },
+      price: {
+        all: [
+          {alias: "$", title: "$"},
+          {alias: "$$", title: "$$"},
+          {alias: "$$$", title: "$$$"},
+          {alias: "$$$$", title: "$$$$"},
+        ],
+        selected: [],
+      },
+    },
+  };
+
+  componentDidMount() {
+    let dropdown = {...this.state.dropdown};
+
+    function getNeighborhoods() {
+      return API.get("/neighborhood");
     }
+
+    function getCategories() {
+      return API.get("/categories");
+    }
+
+    Promise.all([getNeighborhoods(), getCategories()]).then((results) => {
+      const neighborhoods = results[0].data;
+      const categories = results[1].data;
+      dropdown.neighborhoods.all = neighborhoods.sort((a, b) => {
+        let fa = a.title.toLowerCase(),
+          fb = b.title.toLowerCase();
+
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
+      dropdown.categories.all = categories.sort((a, b) => {
+        let fa = a.title.toLowerCase(),
+          fb = b.title.toLowerCase();
+
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
+
+      this.setState({dropdown: dropdown});
+    });
   }
 
   toggleHandler = (type, i) => {
-    const index = (typeof i == "undefined") ? 0 : i;
+    const index = typeof i == "undefined" ? 0 : i;
     let restaurants = [...this.state.restaurants];
     let restaurant = restaurants[index];
-    restaurant.userInteractions[type] = { date: Date.now(), bool: !restaurant.userInteractions[type]['bool'] };
-    this.setState({ restaurants: restaurants });
-  }
+    restaurant.userInteractions[type] = {
+      date: Date.now(),
+      bool: !restaurant.userInteractions[type]["bool"],
+    };
+    this.setState({restaurants: restaurants});
+  };
 
   handleSelectChange = (event) => {
     const target = event.target;
@@ -192,45 +223,42 @@ class SearchPage extends Component {
     const value = target.value;
     const name = target.name;
 
-    let dropdown = { ...this.state.dropdown };
-    dropdown[name]['selected'] = value;
-    this.setState({ dropdown: dropdown });
-  }
+    let dropdown = {...this.state.dropdown};
+    dropdown[name]["selected"] = value;
+    this.setState({dropdown: dropdown});
+  };
 
   handleDelete = (event) => {
-    console.log('Delete Clicked')
+    console.log("Delete Clicked");
     const target = event.target;
     const key = target.key;
     const name = target.name;
 
-    let dropdown = { ...this.state.dropdown };
+    let dropdown = {...this.state.dropdown};
 
-    const index = dropdown[name]['selected'].indexOf(key);
+    const index = dropdown[name]["selected"].indexOf(key);
     if (index > -1) {
-      dropdown[name]['selected'].splice(index, 1);
+      dropdown[name]["selected"].splice(index, 1);
     }
 
-    
-
-    this.setState({ dropdown: dropdown });
-  }
+    this.setState({dropdown: dropdown});
+  };
 
   render() {
     return (
-
       <div>
-
         <SearchDropdowns
           dropdown={this.state.dropdown}
           handleChange={this.handleSelectChange}
-          handleDelete={this.handleDelete} />
+          handleDelete={this.handleDelete}
+        />
 
         <SearchResults
           restaurants={this.state.restaurants}
           toggleHandler={this.toggleHandler}
         />
         {/* <p>{this.state.dropdown}</p> */}
-      </div >
+      </div>
     );
   }
 }
