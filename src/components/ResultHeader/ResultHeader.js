750f3d5e5c45355af18db76d8c67ae3f58efb731
Michael Ferguson <mikeferg2011@gmail.com>
2021-01-24 22:08:33 -0600

    minor cleanup

--- src/components/ResultHeader/ResultHeader.js
+++ src/components/ResultHeader/ResultHeader.js
@@ -1,19 +1,18 @@
 import './ResultsHeader.css'
 
 
-function ResultHeader () {
+export default function ResultHeader () {
     return (
         <div className="resultHeader" >
                 <div>Image</div>
                 <div>Name</div>
                 <div>Neighborhood</div>
                 <div>Yelp Rating</div>
+                <div>Price</div>
                 <div>Categories</div>
                 <div>Yelp Page</div>
                 <div>Wish List</div>
-                <div>Saved</div>
+                <div>Visited</div>
         </div>
     );
 }
-
-export default ResultHeader;
