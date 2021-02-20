18046c54b7effe06d76e5dc47508d7d444949104
Ferg <mikeferg2011@gmail.com>
2021-02-20 16:14:29 -0600

    add user to db on login

--- src/components/SignInOutButton/SignInButton.js
+++ src/components/SignInOutButton/SignInButton.js
@@ -1,17 +1,23 @@
 import { useMsal } from "@azure/msal-react";
 import Button from "@material-ui/core/Button";
-
+import API from '../../utils/Api'
 
 export const SignInButton = () => {
     const { instance } = useMsal();
 
     const handleLogin = () => {
 
-            instance.loginPopup().then(response => {
-                sessionStorage.setItem('logInResponse', JSON.stringify(response.account))
-                window.location.reload();
-                // props.setAccount(response)
-            });
+        instance.loginPopup().then(response => {
+            sessionStorage.setItem('logInResponse', JSON.stringify(response.account))
+            window.location.reload();
+            // props.setAccount(response)
+            const {name, username} = response.account
+            API
+                .post("/user/login", {name: name, email: username})
+                .catch(function (error) {
+                    console.log(error);
+                });
+        });
     }
 
     return (
